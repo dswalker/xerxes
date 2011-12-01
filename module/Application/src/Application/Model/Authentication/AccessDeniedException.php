3f6e03aa08a86df40d325672f7342c8fecd6f02e
David Walker <dwalker@calstate.edu>
2011-12-01 05:31:11 -0800

    why are these not versioned?

---
+++ module/Application/src/Application/Model/Authentication/AccessDeniedException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application\Model\Authentication;
+
+/**
+ * Access Denied Exception
+ * 
+ * @author David Walker
+ * @author John Blyberg
+ * @link http://xerxes.calstate.edu
+ * @license http://www.gnu.org/licenses/
+ * @version $Id$
+ * @package Xerxes
+ */
+
+class AccessDeniedException extends \Exception
+{
+	
+}
