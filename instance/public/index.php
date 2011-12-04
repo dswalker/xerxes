54448b283182e4086e5e3017b08f1c69910d3af0
David Walker <dwalker@calstate.edu>
2011-12-04 09:35:37 -0800

    moving the startup code to instance

--- instance/public/index.php
+++ instance/public/index.php
@@ -1,21 +1,3 @@
 <?php
-require_once dirname(__DIR__) . '/vendor/Zend/Loader/AutoloaderFactory.php';
-Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
 
-$appConfig = include dirname(__DIR__) . '/config/application.config.php';
-
-$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
-$moduleLoader->register();
-
-$moduleManager = new Zend\Module\Manager($appConfig['modules']);
-$listenerOptions = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
-$moduleManager->setDefaultListenerOptions($listenerOptions);
-$moduleManager->getConfigListener()->addConfigGlobPath(dirname(__DIR__) . '/config/autoload/*.config.php');
-$moduleManager->loadModules();
-
-// Create application, bootstrap, and run
-$bootstrap   = new Zend\Mvc\Bootstrap($moduleManager->getMergedConfig());
-
-$application = new Zend\Mvc\Application;
-$bootstrap->bootstrap($application);
-$application->run()->send();
+require_once '../run.php';
---
+++ instance/run.php
@@ -0,0 +1,24 @@
+<?php
+
+chdir(__DIR__);
+
+require_once dirname(__DIR__) . '/vendor/Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
+
+$appConfig = include dirname(__DIR__) . '/config/application.config.php';
+
+$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
+$moduleLoader->register();
+
+$moduleManager = new Zend\Module\Manager($appConfig['modules']);
+$listenerOptions = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
+$moduleManager->setDefaultListenerOptions($listenerOptions);
+$moduleManager->getConfigListener()->addConfigGlobPath(dirname(__DIR__) . '/config/autoload/*.config.php');
+$moduleManager->loadModules();
+
+// Create application, bootstrap, and run
+$bootstrap   = new Zend\Mvc\Bootstrap($moduleManager->getMergedConfig());
+
+$application = new Zend\Mvc\Application;
+$bootstrap->bootstrap($application);
+$application->run()->send();
