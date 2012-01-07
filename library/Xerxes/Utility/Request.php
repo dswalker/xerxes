e01ee88841c7bd74822ba75e1372855377a262e0
David Walker <dwalker@calstate.edu>
2012-01-06 16:24:46 -0800

    formatting

--- library/Xerxes/Utility/Request.php
+++ library/Xerxes/Utility/Request.php
@@ -54,172 +54,172 @@ class Request extends ZendRequest
 	 * @param RouteStack $router
 	 */
 	
-    public function setRouter(RouteStack $router)
-    {
-    	$this->router = $router;
-    	
-        // now extract the route elements and set them as params
-        
-        $route_match = $router->match($this);
-        
-        if ( $route_match instanceof RouteMatch )
-        {
-	        foreach ($route_match->getParams() as $name => $value )
-	        {
-	        	$this->setParam($name, $value);
-	        }
-        }
-    }
-    
-    /**
-     * Add the Controller Map 
-     * 
-     * This is really just convenience so other parts of the system can
-     * easily gain access to it
-     * 
-     * @param ControllerMap $controller_map
-     */
-    
-    public function setControllerMap(ControllerMap $controller_map )
-    {
-    	$this->controller_map = $controller_map;
-    }
-    
-    /**
-     * Get the Controller Map
-     * 
-     * @throws \Exception		if no controller map previous set
-     * @return ControllerMap
-     */
-    
-    public function getControllerMap()
-    {
-    	if ( ! $this->controller_map instanceof ControllerMap )
-    	{
-    		throw new \Exception("No controller map set");
-    	}
-    	
-    	return $this->controller_map;
-    }
-    
-    /**
-     * Set the session manager
-     *
-     * @param  Manager $manager
-     * @return FlashMessenger
-     */
-    
-    public function setSessionManager(Manager $manager)
-    {
-    	$this->session = $manager;
-    	return $this;
-    }
-    
-    /**
-     * Retrieve the session manager
-     *
-     * If none composed, lazy-loads a SessionManager instance
-     *
-     * @return Manager
-     */
-    
-    public function session()
-    {
-    	if (!$this->session instanceof Manager)
-    	{
-    		$this->setSessionManager(new SessionManager());
-    	}
-    	
-    	return $this->session;
-    }   
-    
-    /**
-     * Get session container
-     * 
-     * @param string $name		[optional] id for the container, default is Public
-     *
-     * @return Container
-     */
-    
-    public function getContainer($name = "Public")
-    {
-    	// got one already?
-    	
-    	if ( array_key_exists($name, $this->containers) )
-    	{
-	    	if ($this->containers[$name] instanceof Container) 
-	    	{
-	    		return $this->containers[$name];
-	    	}
-    	}
-    	
-    	// make a new one
-    
-    	$manager = $this->session();
-    	$container = new Container($name, $manager);
-    	
-    	$this->containers[$name] = $container; // set for later
-
-    	return $container;
-    }    
-    
+	public function setRouter(RouteStack $router)
+	{
+		$this->router = $router;
+		
+		// now extract the route elements and set them as params
+		
+		$route_match = $router->match($this);
+		
+		if ( $route_match instanceof RouteMatch )
+		{
+			foreach ($route_match->getParams() as $name => $value )
+			{
+				$this->setParam($name, $value);
+			}
+		}
+	}
+	
+	/**
+	 * Add the Controller Map 
+	 * 
+	 * This is really just convenience so other parts of the system can
+	 * easily gain access to it
+	 * 
+	 * @param ControllerMap $controller_map
+	 */
+	
+	public function setControllerMap(ControllerMap $controller_map )
+	{
+		$this->controller_map = $controller_map;
+	}
+	
+	/**
+	 * Get the Controller Map
+	 * 
+	 * @throws \Exception		if no controller map previous set
+	 * @return ControllerMap
+	 */
+	
+	public function getControllerMap()
+	{
+		if ( ! $this->controller_map instanceof ControllerMap )
+		{
+			throw new \Exception("No controller map set");
+		}
+		
+		return $this->controller_map;
+	}
+	
+	/**
+	 * Set the session manager
+	 *
+	 * @param  Manager $manager
+	 * @return FlashMessenger
+	 */
+	
+	public function setSessionManager(Manager $manager)
+	{
+		$this->session = $manager;
+		return $this;
+	}
+	
+	/**
+	 * Retrieve the session manager
+	 *
+	 * If none composed, lazy-loads a SessionManager instance
+	 *
+	 * @return Manager
+	 */
+	
+	public function session()
+	{
+		if (!$this->session instanceof Manager)
+		{
+			$this->setSessionManager(new SessionManager());
+		}
+		
+		return $this->session;
+	}
+	
+	/**
+	 * Get session container
+	 * 
+	 * @param string $name		[optional] id for the container, default is Public
+	 *
+	 * @return Container
+	 */
+	
+	public function getContainer($name = "Public")
+	{
+		// got one already?
+		
+		if ( array_key_exists($name, $this->containers) )
+		{
+			if ($this->containers[$name] instanceof Container) 
+			{
+				return $this->containers[$name];
+			}
+		}
+		
+		// make a new one
+		
+		$manager = $this->session();
+		$container = new Container($name, $manager);
+		
+		$this->containers[$name] = $container; // set for later
+		
+		return $container;
+	}	
+	
 	/**
 	 * Add value to Session
 	 * 
 	 * @param string $key
 	 * @param mixed $value
 	 */
-    
-    public function setSessionData($key, $value)
-    {
-    	$this->getContainer()->offsetSet($key, $value);
-    }
-    
-    /**
-     * Unset a value in Session
-     *
-     * @param string $key
-     */
-    
-    public function unsetSessionData($key)
-    {
-   		$this->getContainer()->offsetUnset($key);
-    }    
-    
-    /**
-     * Check if a key is set in Session
-     *
-     * @param string $key
-     */
-    
-    public function existsInSessionData($key)
-    {
-    	$this->getContainer()->offsetExists($key);
-    }    
-    
-    /**
-     * Get session value
-     * 
-     * @param string $key
-     * @return mixed 		value, if key exists, otherwise null
-     */
-    
-    public function getSessionData($key)
-    {
-    	return $this->getContainer()->offsetGet($key);
-    }
-    
-    /**
-     * Get all session values
-     * 
-     * @return array
-     */
-    
-    public function getAllSessionData()
-    {
-    	return $this->getContainer()->getIterator()->getArrayCopy();
-    }
-    
+	
+	public function setSessionData($key, $value)
+	{
+		$this->getContainer()->offsetSet($key, $value);
+	}
+	
+	/**
+	 * Unset a value in Session
+	 *
+	 * @param string $key
+	 */
+	
+	public function unsetSessionData($key)
+	{
+		$this->getContainer()->offsetUnset($key);
+	}	
+	
+	/**
+	 * Check if a key is set in Session
+	 *
+	 * @param string $key
+	 */
+	
+	public function existsInSessionData($key)
+	{
+		$this->getContainer()->offsetExists($key);
+	}	
+	
+	/**
+	 * Get session value
+	 * 
+	 * @param string $key
+	 * @return mixed 		value, if key exists, otherwise null
+	 */
+	
+	public function getSessionData($key)
+	{
+		return $this->getContainer()->offsetGet($key);
+	}
+	
+	/**
+	 * Get all session values
+	 * 
+	 * @return array
+	 */
+	
+	public function getAllSessionData()
+	{
+		return $this->getContainer()->getIterator()->getArrayCopy();
+	}
+	
 	/**
 	 * Process the incoming request paramaters
 	 */
@@ -477,9 +477,9 @@ class Request extends ZendRequest
 	 * 
 	 * @param string $regex			[optional] regular expression for properties to get
 	 * @param bool $shrink			[optional] whether to collapse properties stored as 
-	 * 											array into simple element, default false
+	 *   array into simple element, default false
 	 * @param string $shrink_del 	[optional] if $shrink is true, then separate multiple 
-	 *											elements by this character, default comma
+	 *   elements by this character, default comma
 	 * 
 	 * @return array
 	 */
@@ -513,7 +513,7 @@ class Request extends ZendRequest
 					{
 						continue;
 					}
-				}				
+				}
 				
 				if ( $value == "")
 				{
@@ -576,37 +576,37 @@ class Request extends ZendRequest
 		{
 			return '';
 		}
-	
-	
+		
+		
 		// this only returns the route, no querystring
-	
+		
 		$url = $this->router->assemble($params, $options);
-	
+		
 		// remove trailing '/index' from generated URLs.
-	
+		
 		if ((6 <= strlen($url)) && '/index' == substr($url, -6))
 		{
 			$url = substr($url, 0, strlen($url) - 6);
 		}
-	
-	
+		
+		
 		// append query string
-	
+		
 		$query_string = array();
-	
+		
 		// figure out which of our params matched the route
-	
+		
 		$request = new Request();
 		$request->setUri($url);
-	
+		
 		$route_match = $this->router->match($request);
-	
+		
 		// remove those from the supplied params
-	
+		
 		if ( $route_match instanceof RouteMatch )
 		{
 			$in_route = array_keys($route_match->getParams());
-	
+			
 			foreach ( $params as $key => $value )
 			{
 				if ( ! in_array($key, $in_route) )
@@ -615,41 +615,41 @@ class Request extends ZendRequest
 				}
 			}
 		}
-	
+		
 		// take any remaining as the query string
-	
+		
 		if ( count($query_string) > 0 )
 		{
 			$url .= "?";
-	
+			
 			$x = 0;
-	
+			
 			foreach ( $query_string as $name => $value )
 			{
 				if ( $value == "" )
 				{
 					continue;
 				}
-	
+				
 				if ( $x > 0 ) // first param doesn't need & prefix
 				{
 					$url .= '&';
 				}
-	
+				
 				$url .= $name . '=' . urlencode($value);
-	
+				
 				$x++;
 			}
 		}
-	
+		
 		// is it supposed to be a full url?
-	
+		
 		if ( $full == true )
 		{
 			$base = $this->getServerUrl($force_secure);
 			$url = $base .= $url;
 		}
-	
+		
 		return $url;
 	}
 	
