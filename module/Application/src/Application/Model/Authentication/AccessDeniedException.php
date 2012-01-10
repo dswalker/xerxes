0ab032b5a5b1d0eccfd31fa40a60134c56ffc8be
David Walker <dwalker@calstate.edu>
2012-01-10 11:51:38 -0800

    comments

--- module/Application/src/Application/Model/Authentication/AccessDeniedException.php
+++ module/Application/src/Application/Model/Authentication/AccessDeniedException.php
@@ -6,7 +6,7 @@ namespace Application\Model\Authentication;
  * Access Denied Exception
  * 
  * @author David Walker
- * @author John Blyberg
+ * @copyright 2012 California State University
  * @link http://xerxes.calstate.edu
  * @license http://www.gnu.org/licenses/
  * @version
