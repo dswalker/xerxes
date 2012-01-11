7d6599b9a247c27aff067d0a357a081903912349
David Walker <dwalker@calstate.edu>
2012-01-11 09:16:50 -0800

    comments

--- library/Xerxes/Utility/Xsl.php
+++ library/Xerxes/Utility/Xsl.php
@@ -9,9 +9,9 @@ namespace Xerxes\Utility;
  * @author Jonathan Rochkind
  * @copyright 2011 California State University
  * @link http://xerxes.calstate.edu
- * @license http://www.gnu.org/licenses/
+ * @license
  * @version
- * @package  Xerxes_Framework
+ * @package  Xerxes_Utility
  */ 
 
 class Xsl
