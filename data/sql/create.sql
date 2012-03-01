0c505a4dfb5c16423e2340ce4a2f97acc604321c
David Walker <dwalker@calstate.edu>
2012-03-01 06:05:56 -0800

    sql updates

--- data/sql/create.sql
+++ data/sql/create.sql
@@ -16,8 +16,9 @@ DROP TABLE IF EXISTS xerxes_subcategories;
 DROP TABLE IF EXISTS xerxes_databases;
 DROP TABLE IF EXISTS xerxes_categories;
 DROP TABLE IF EXISTS xerxes_types;
+DROP TABLE IF EXISTS xerxes_reading_list;
+DROP TABLE IF EXISTS xerxes_search_stats;
 DROP TABLE IF EXISTS xerxes_user_usergroups;
-DROP TABLE IF EXISTS xerxes_cache_alternate_id;
 DROP TABLE IF EXISTS xerxes_cache;
 DROP TABLE IF EXISTS xerxes_tags;
 DROP TABLE IF EXISTS xerxes_sfx;
@@ -195,4 +196,23 @@ CREATE TABLE xerxes_user_subcategory_databases(
 
 	PRIMARY KEY(database_id, subcategory_id),
 	FOREIGN KEY (subcategory_id) REFERENCES xerxes_user_subcategories (id) ON DELETE CASCADE
+);
+
+CREATE TABLE xerxes_reading_list (
+	id 		MEDIUMINT NOT NULL AUTO_INCREMENT,
+	context_id	VARCHAR(20),
+	record_id	MEDIUMINT,
+	record_order	INTEGER,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (record_id) REFERENCES xerxes_records(id) ON DELETE CASCADE
+);
+
+CREATE TABLE xerxes_search_stats (
+	ip_address	VARCHAR(20),
+	stamp		TIMESTAMP,
+	module		VARCHAR(20),
+	field		VARCHAR(20),
+	phrase		VARCHAR(1000),
+	hits		INTEGER
 );
