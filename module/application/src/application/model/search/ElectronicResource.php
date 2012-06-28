947a4c65979844d5edcaf38f3045c1a5c99568b2
David Walker <dwalker@calstate.edu>
2012-06-28 10:57:56 -0700

    clean-up take 2

---
+++ module/application/src/application/model/search/ElectronicResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Model\Search;
+
+use Xerxes\Utility\Parser;
+
+/**
+ * Electronic Resource
+ * 
+ * @todo: merge this with Knowledgebase\Database?
+ *
+ * @author David Walker
+ * @copyright 2012 California State University
+ * @link http://xerxes.calstate.edu
+ * @license http://www.gnu.org/licenses/
+ * @version
+ * @package Xerxes
+ */
+
+class ElectronicResource
+{
+	public $database;
+	public $coverage;
+	public $link;
+	public $package;
+}
