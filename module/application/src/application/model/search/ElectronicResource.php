5ec8ed6d3cd44b2be9e64374ffdb055ac05500d8
David Walker <dwalker@calstate.edu>
2012-06-28 07:16:53 -0700

    simplify electronic resource

--- module/application/src/application/model/search/ElectronicResource.php
+++ module/application/src/application/model/search/ElectronicResource.php
@@ -17,65 +17,8 @@ use Xerxes\Utility\Parser;
 
 class ElectronicResource
 {
-	protected $database;
-	protected $coverage;
-	protected $link;
-	protected $package;	
-	
-	/**
-	 * Set a property for this item
-	 * 
-	 * @param string $name		property name
-	 * @param mixed $value		the value
-	 */
-    
-	public function setProperty($name, $value)
-	{
-		if ( property_exists($this, $name) )
-		{
-			$this->$name = $value;
-		}
-	}
-
-	/**
-	 * Get a property from this item
-	 * 
-	 * @param string $name		property name
-	 * @return mixed the value
-	 */
-	
-	public function getProperty($name)
-	{
-		if ( property_exists($this, $name) )
-		{
-			return $this->$name;
-		}
-		else
-		{
-			throw new \Exception("trying to access propety '$name', which does not exist");
-		}
-	}
-	
-	/**
-	 * Serialize to Array
-	 * 
-	 * @return array
-	 */
-	
-	public function toArray()
-	{
-		$array = array();
-		
-		foreach ( $this as $key => $value )
-		{
-			if ( $value == "")
-			{
-				continue;
-			}
-			
-			$array[$key] = $value;
-		}
-		
-		return $array;
-	}
+	public $database;
+	public $coverage;
+	public $link;
+	public $package;
 }
