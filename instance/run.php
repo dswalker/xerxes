8594501272be74b214a8f93ac4fd1bef1a63854a
David Walker <dwalker@calstate.edu>
2012-07-17 06:48:15 -0700

    bug fix, path is only one level up

--- instance/run.php
+++ instance/run.php
@@ -5,7 +5,7 @@ Zend\Mvc\Service\ServiceManagerConfiguration;
 
 chdir(__DIR__);
 
-$path = dirname(dirname(__DIR__));
+$path = dirname(__DIR__);
 
 // Composer autoloading
 if (!include_once("$path/vendor/autoload.php")) {
