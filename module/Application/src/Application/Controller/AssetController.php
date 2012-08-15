e9459a29c532b34687cc12f2a74931b5a2658373
David Walker <dwalker@calstate.edu>
2012-08-15 05:52:10 -0700

    set labels template in controller

--- module/Application/src/Application/Controller/AssetController.php
+++ module/Application/src/Application/Controller/AssetController.php
@@ -16,22 +16,9 @@ class AssetController extends ActionController
 		$labels = $this->locator->get('labels');
 		$labels->setLanguage($lang);
 		
-		return array("labels" => $labels);
-	}
-	
-	public function testAction()
-	{
-		$test = array('hello' => 'world');
-		$json = json_encode($test);
-		
-		header("Content-type: application/json"); echo $json; exit;
-		
-		$this->request->setParam("format", "json");
-		
 		$model = new ViewModel();
-		$model->setVariable('json', $json);
-		
-		$this->request->getControllerMap()->setNoView();
+		$model->setVariable('labels', $labels);
+		$model->setTemplate('asset/labels.phtml');
 		
 		return $model;
 	}
