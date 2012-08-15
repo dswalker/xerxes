86644a9bfbd3297351f200f1c566e54304dc6e39
David Walker <dwalker@calstate.edu>
2012-08-15 05:48:44 -0700

    email utility

---
+++ module/Application/src/Xerxes/Utility/Email.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Xerxes\Utility;
+
+use Zend\Mail\Transport\SmtpOptions;
+use Zend\Mail\Transport\Sendmail;
+use Zend\Mail\Message;
+
+/**
+ * Email
+ * 
+ * @author David Walker
+ * @copyright 2012 California State University
+ * @link
+ * @license
+ * @version
+ */
+
+class Email 
+{
+	private $from;
+	private $host;
+	private $transport;
+	
+	public function __construct()
+	{
+		$registry = Registry::getInstance();
+		
+		$this->from = $registry->getConfig("EMAIL_FROM", true);
+		$this->host = $registry->getConfig("SMTP_SERVER", false);
+		
+		if ( strstr($this->host, ':') )
+		{
+			$parts = explode(':', $this->host);
+			$this->port = array_pop($parts);
+			$this->host = implode(':', $parts);
+		}
+		
+		if ( $this->host != '' )
+		{
+			$options = new SmtpOptions();
+			$options->setHost($this->host);
+			$options->setPort($this->port);
+		
+			$this->transport = new Sendmail($options);
+		}
+		else
+		{
+			$this->transport = new Sendmail();
+		}		
+	}
+	
+	public function send($email, $subject, $body)
+	{
+		$message = new Message();
+		
+		$message->setTo($email);
+		$message->setFrom($this->from);
+		$message->setSubject($subject);
+		$message->setBody($body);
+		
+		$this->transport->send($message);
+	}
+}
