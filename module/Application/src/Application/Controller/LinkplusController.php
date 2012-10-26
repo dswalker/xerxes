ac5eaa3ab17b908cb9be5fc5147943e5805cb477
David Walker <dwalker@calstate.edu>
2012-10-26 07:49:45 -0700

    hacky local csu ars request

--- module/Application/src/Application/Controller/LinkplusController.php
+++ module/Application/src/Application/Controller/LinkplusController.php
@@ -2,6 +2,7 @@
 
 namespace Application\Controller;
 
+use Application\Model\Search\Availability\Innopac\Config;
 use Zend\Mvc\Controller\ActionController;
 
 class LinkplusController extends SolrController
@@ -47,5 +48,34 @@ class LinkplusController extends SolrController
 		$url = "http://csul.iii.com/search/$index?SEARCH=" . urlencode($query);
 		
 		return $this->redirect()->toUrl($url);		
+	}
+	
+	public function requestAction()
+	{
+		$id = $this->request->getParam("id");
+		
+		$config = Config::getInstance();
+		
+		$server = $config->getConfig('SERVER', true);
+		$server = rtrim($server, '/');
+	
+		$url = "$server/record=$id";
+		
+		$html = file_get_contents($url);
+	
+		$arrMatch = array();
+		$redirect = "";
+	
+		$pattern = '/href="(.*asrsreq[^"]*)"/';
+	
+		if ( preg_match_all($pattern, $html, $arrMatch) != 1 )
+		{
+			return $this->redirect()->toUrl($url);
+		}
+		elseif ( preg_match($pattern, $html, $arrMatch) )
+		{
+			$redirect = $server . $arrMatch[1];
+			return $this->redirect()->toUrl($redirect);
+		}
 	}
 }
