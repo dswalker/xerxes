d9a527ee0a296baf30ec34733df0c16251f402b2
David Walker <dwalker@calstate.edu>
2012-12-18 14:11:23 -0800

    solr view helper

---
+++ module/Application/src/Application/View/Helper/Solr.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\View\Helper;
+
+use Xerxes\Record\Author;
+
+class Solr extends Search
+{
+	/**
+	 * Make sure author names are based on full (display) field and quoted
+	 * 
+	 * @see Application\View\Helper.Search::linkAuthor()
+	 */
+	
+	public function linkAuthor( Author $author )
+	{
+		$query = $author->getName();
+		
+		if ( $author->display != "" )
+		{
+			$query = $author->display;
+		}
+		
+		$arrParams = $this->lateralLink();
+		$arrParams['field'] = 'author';
+		$arrParams['query'] = "\"$query\"";
+
+		return $this->request->url_for($arrParams);
+	}
+}
