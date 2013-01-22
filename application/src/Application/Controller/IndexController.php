a2b1d5506bac81c01a265fef4a5160ff45795e1a
David Walker <dwalker@calstate.edu>
2013-01-21 19:59:22 -0800

    simplified index controller

--- application/src/Application/Controller/IndexController.php
+++ application/src/Application/Controller/IndexController.php
@@ -2,42 +2,24 @@
 
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\ActionController;
+use Xerxes\Mvc\ActionController;
 
 class IndexController extends ActionController
 {
 	public function indexAction()
 	{
-		if ( $this->request->getParam('base') != '')
-		{
-			$params = $this->request->getParams();
-			
-			$params['controller'] = $params['base'];
-			unset($params['base']);
-			
-			$url = $this->request->url_for($params);
-			
-			//print_r($params); echo "<p>$url</p>"; exit;
-			
-			return $this->redirect()->toUrl($url);
-		}
-		
-		
-		
-		
-		$controller_map = $this->request->getControllerMap();
-		
 		// get the default controller configured in map
 		
-		$controller = $controller_map->getDefaultController();
+		$default_controller = $this->controller_map->getDefaultController();
 		
-		// update the controller map and request
+		// update the request
 		
-		$controller_map->setController($controller);
-		$this->request->replaceParam('controller', $controller);
+		$this->request->replaceParam('controller', $default_controller);
 		
 		// invoke the controller
 		
-		return $this->forward()->dispatch($controller);
+		$controller = $this->controller_map->getControllerObject($default_controller, $this->event);
+		
+		return $controller->execute('index');
 	}
 }
