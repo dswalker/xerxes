8d66f8634cda3530e4719bedfdeab7c82455f679
David Walker <dwalker@calstate.edu>
2013-01-26 05:12:42 -0800

    last few copyright and stuff in comments

--- instances/instance/public/javascript/sms.js
+++ instances/instance/public/javascript/sms.js
@@ -2,11 +2,6 @@
  * books record page
  *
  * @author David Walker
- * @copyright 2011 California State University
- * @link http://xerxes.calstate.edu
- * @license http://www.gnu.org/licenses/
- * @version
- * @package Xerxes
  */
  
  $(document).ready(prepareSMS);
