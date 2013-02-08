6336ef0894cb112ca88669b27797b6501207246f
David Walker <dwalker@calstate.edu>
2013-02-07 19:18:40 -0800

    Alt title and keyword objects

---
+++ application/src/Application/Model/Databases/AlternateTitle.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of Xerxes.
+ *
+ * (c) California State University <library@calstate.edu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\Model\Databases;
+
+/**
+ * Alternate Title
+ *
+ * @author David Walker <dwalker@calstate.edu>
+ * 
+ * @Entity @Table(name="alternate_titles")
+ */
+
+class AlternateTitle 
+{
+	/** @Id @Column(type="integer") @GeneratedValue **/
+	protected $title_id;
+	
+	/**
+	 * @Column(type="string") 
+	 * @var string
+	 */
+	protected $name;
+	
+	/**
+	 * @ManyToOne(targetEntity="Database", inversedBy="alternate_titles")
+	 * @var Database
+	 */
+	protected $database;
+	
+	/**
+	 * @return Database
+	 */
+	public function getDatabase()
+	{
+		return $this->database;
+	}
+
+	/**
+	 * @param Database $database
+	 */
+	public function setDatabase(Database $database) 
+	{
+		$this->database = $database;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name) 
+	{
+		$this->name = $name;
+	}
+}
---
+++ application/src/Application/Model/Databases/Keyword.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Xerxes.
+ *
+ * (c) California State University <library@calstate.edu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\Model\Databases;
+
+/**
+ * Keyword
+ *
+ * @author David Walker <dwalker@calstate.edu>
+ * 
+ * @Entity @Table(name="keywords")
+ */
+
+class Keyword 
+{
+	/** @Id @Column(type="integer") @GeneratedValue **/
+	protected $keyword_id;
+	
+	/**
+	 * @Column(type="string") 
+	 */
+	protected $value;
+	
+	/**
+	 * @ManyToOne(targetEntity="Database", inversedBy="alternate_titles")
+	 */
+	protected $database;
+}
