bcf04de37c5b00f8b13be436907587f7be6aff71
David Walker <dwalker@calstate.edu>
2014-06-10 23:30:32 -0700

    reset migrate script to nothing

--- data/sql/migrate/migrate-1.8-to-2.0.sql
+++ data/sql/migrate/migrate-1.8-to-2.0.sql
@@ -1,36 +1,3 @@
-#DROP TABLE IF EXISTS xerxes_search_stats;
-DROP TABLE IF EXISTS xerxes_reading_list;
-DROP TABLE IF EXISTS xerxes_reading_list_users;
-
-CREATE TABLE xerxes_reading_list (
-	id 		MEDIUMINT NOT NULL AUTO_INCREMENT,
-	context_id	VARCHAR(20),
-	record_id	MEDIUMINT,
-	record_order	INTEGER,
-	title		VARCHAR(1000),
-	author		VARCHAR(500),
-	publication	VARCHAR(1000),
-	description	TEXT,
-
-	PRIMARY KEY (id),
-	FOREIGN KEY (record_id) REFERENCES xerxes_records(id) ON DELETE CASCADE
-);
-
-CREATE TABLE xerxes_reading_list_users (
-	id 		VARCHAR(50),
-	username	VARCHAR(50),		
-
-	PRIMARY KEY (id),
-	FOREIGN KEY (username) REFERENCES xerxes_users(username) ON DELETE CASCADE
-);
-
-#CREATE TABLE xerxes_search_stats (
-#	ip_address	VARCHAR(20),
-#	stamp		TIMESTAMP,
-#	module		VARCHAR(20),
-#	field		VARCHAR(20),
-#	phrase		VARCHAR(1000),
-#	hits		INTEGER
-#);
-
-#ALTER TABLE xerxes_records MODIFY original_id VARCHAR(255);
+DROP TABLE IF EXISTS xerxes_user_subcategory_databases;
+DROP TABLE IF EXISTS xerxes_user_subcategories;
+DROP TABLE IF EXISTS xerxes_user_categories;
